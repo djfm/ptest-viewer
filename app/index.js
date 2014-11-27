b32f7f039a5802d90c585724582d8bbfd1578eca
fram <fm.de.jouvencel@gmail.com>
2014-11-27 16:00:54 +0100

    // make port configurable

--- app/index.js
+++ app/index.js
@@ -65,7 +65,7 @@ app.get('/screencasts/:testToken/:which?', function (req, res) {
 	
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.argv[3] || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
